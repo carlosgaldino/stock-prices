c1f14855b29b2f8acb67f2e27400ef934f26380a
Carlos Galdino <carloshsgaldino@gmail.com>
2024-04-30 21:33:21 +0100

    Output prices in GBP

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use surf;
 const URL: &str = "https://query1.finance.yahoo.com/v8/finance/chart/";
 
 #[derive(Debug, serde::Deserialize)]
@@ -27,10 +26,14 @@ struct Metadata {
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let file = std::env::args()
-        .skip(1)
-        .next()
+        .nth(1)
         .ok_or_else(|| std::io::Error::new(std::io::ErrorKind::InvalidInput, "missing argument"))?;
     let file = std::fs::read_to_string(file)?;
+    let format = time::macros::format_description!("[year]-[month]-[day]");
+    let date = time::OffsetDateTime::now_utc().format(format)?;
+    let req = surf::get(format!("{}{}", URL, "USDGBP=X"));
+    let Chart { chart } = surf::client().recv_json(req).await?;
+    let usd_price = chart.result[0].meta.regular_market_price;
     for line in file.lines() {
         let mut tokens = line.split_ascii_whitespace();
         let symbol = tokens.next().ok_or_else(|| {
@@ -39,7 +42,24 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         let beancount_symbol = tokens.next();
         let req = surf::get(format!("{}{}", URL, symbol));
         let Chart { chart } = surf::client().recv_json(req).await?;
-        dbg!(&chart);
+        let meta = &chart.result[0].meta;
+        let symbol = if let Some(beancount_symbol) = beancount_symbol {
+            beancount_symbol
+        } else {
+            &meta.symbol
+        };
+        let price_conv = match meta.currency.as_str() {
+            "GBp" => 0.01,
+            "GBP" => 1.0,
+            "USD" => usd_price,
+            _ => unreachable!("unknown currency {:?}", chart),
+        };
+        println!(
+            "{} price {} {} GBP",
+            date,
+            symbol,
+            meta.regular_market_price * price_conv,
+        );
     }
 
     Ok(())
